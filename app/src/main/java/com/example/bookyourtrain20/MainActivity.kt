e4f69d2c342e192bdbd3facb87461647c73cf275
imrismaa <rismasaputri@mail.ugm.ac.id>
2023-12-05 20:35:26 +0700

    apply Tab Layout

--- app/src/main/java/com/example/bookyourtrain20/MainActivity.kt
+++ app/src/main/java/com/example/bookyourtrain20/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.bookyourtrain20
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.example.bookyourtrain20.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        lateinit var viewPagers: ViewPager2
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            viewPager.adapter = TabAdapter(this@MainActivity)
+
+            TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+                tab.text = when (position) {
+                    0 -> "Register"
+                    1 -> "Login"
+                    else -> throw IllegalArgumentException("Position out of array")
+                }
+            }.attach()
+            viewPagers = viewPager
+        }
     }
 }
